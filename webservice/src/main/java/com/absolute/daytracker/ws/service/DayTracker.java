8d24e2e0c0b5f6e39a01c7ff4ae67b68f23c1602
Andrew Won <andyjwon@gmail.com>
2013-05-21 09:51:59 -0700

    Updated pom.xml with Google API dependencies and created empty shell for GCal test

---
+++ webservice/src/main/java/com/absolute/daytracker/ws/service/DayTracker.java
@@ -0,0 +1,11 @@
+package com.absolute.daytracker.ws.service;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+
+public class DayTracker {
+
+}
